34efaf0f32f57738d75117aaedbe8b3bd4c2cfbd
Gangesh54321 <gangesh54321@gmail.com>
2024-03-02 20:11:12 +0530

    Change done

--- AppiumDemo/src/test/java/Demo/DragAndDropDemo.java
+++ AppiumDemo/src/test/java/Demo/DragAndDropDemo.java
@@ -22,21 +22,17 @@ public class DragAndDropDemo {
 		
 		DesiredCapabilities capabilities = new DesiredCapabilities();
 		
-		   capabilities.setCapability("deviceName","OnePlus AC2001");
-	        capabilities.setCapability("platformname", "Android");     
-	        capabilities.setCapability("automationName","uiautomator2");
-	        capabilities.setCapability("platformversion", "12");
+		capabilities.setCapability("deviceName","Gangesh");
+        capabilities.setCapability("platformname", "Android");     
+        capabilities.setCapability("automationName","uiautomator2");
+        capabilities.setCapability("platformversion", "12");
 	        //io.appium.android.apis/io.appium.android.apis.ApiDemos
 	        capabilities.setCapability("appPackage","io.appium.android.apis");
 	        capabilities.setCapability("appActivity", "io.appium.android.apis.ApiDemos");
 	        
 
-	        URL url = URI.create("http://127.0.0.1:4723/").toURL();
-	        
-	        AndroidDriver driver = new AndroidDriver(url, capabilities);
-	       
-	        Thread.sleep(2000);
-	       
+	        AndroidDriver driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+	        Thread.sleep(5000);
 	        System.out.println("Application Started");
 	        
 	        
