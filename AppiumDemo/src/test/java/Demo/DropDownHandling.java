f0067ce7fe9660b2848d6eaa2a261cb83e32d411
Gangesh54321 <gangesh54321@gmail.com>
2024-03-03 11:47:17 +0530

    DOM update

--- AppiumDemo/src/test/java/Demo/DropDownHandling.java
+++ AppiumDemo/src/test/java/Demo/DropDownHandling.java
@@ -20,21 +20,18 @@ public class DropDownHandling {
 
 		DesiredCapabilities capabilities = new DesiredCapabilities();
 
-		capabilities.setCapability("deviceName","OnePlus AC2001");
-		capabilities.setCapability("platformname", "Android");     
-		capabilities.setCapability("automationName","uiautomator2");
-		capabilities.setCapability("platformversion", "12");
+		capabilities.setCapability("deviceName","Gangesh");
+        capabilities.setCapability("platformname", "Android");     
+        capabilities.setCapability("automationName","uiautomator2");
+        capabilities.setCapability("platformversion", "12");
+	        //io.appium.android.apis/io.appium.android.apis.ApiDemos
+	        capabilities.setCapability("appPackage","io.appium.android.apis");
+	        capabilities.setCapability("appActivity", "io.appium.android.apis.ApiDemos");
+	        
 
-		capabilities.setCapability("appPackage","io.appium.android.apis");
-		capabilities.setCapability("appActivity", "io.appium.android.apis.ApiDemos");
-		//io.appium.android.apis/io.appium.android.apis.ApiDemos
-
-
-		URL url = URI.create("http://127.0.0.1:4723/").toURL();
-
-		AndroidDriver driver = new AndroidDriver(url, capabilities);
-		Thread.sleep(2000);
-		System.out.println("Application Started");
+	        AndroidDriver driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+	        Thread.sleep(5000);
+	        System.out.println("Application Started");
 
 
 		//click on view button
@@ -58,6 +55,7 @@ public class DropDownHandling {
 		Thread.sleep(2000);
 		System.out.println("Session closed");
 		driver.quit();
+		//Sucess fully rum some DOM not detect wait 10 min, after 10 min it will be work.
 	}
 
 }
